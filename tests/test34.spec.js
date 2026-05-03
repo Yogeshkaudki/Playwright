8ae4e0178c0c19e9ad69899cb9607e5470181998
Yogesh <kaudkiyogesh@gmail.com>
2026-05-03 11:18:44 +0530

    added one new file

---
+++ tests/test34.spec.js
@@ -0,0 +1,15 @@
+const {test, expect, request} = require('@playwright/test');
+const {APiUtils} = require('./utils/Apiutils copy');
+const loginPayLoad = {userEmail:"anshika@gmail.com",userPassword:"Iamking@000"};
+const orderPayLoad = {orders:[{country:"Cuba",productOrderedId:"67a8dde5c0d3e6622a297cc8"}]};
+ 
+ 
+let response;
+test.beforeAll( async()=>
+{
+   const apiContext = await request.newContext();
+   const apiUtils = new APiUtils(apiContext,loginPayLoad);
+   response =  await apiUtils.createOrder(orderPayLoad);
+ 
+})
+ 
